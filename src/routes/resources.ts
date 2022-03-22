8f769cc22a71d88be990d830e176d5e8f05a490c
Andrei Dascălu <andrew.dascalu@gmail.com>
2022-03-22 22:55:16 +0100

    expose resource info

--- src/routes/resources.ts
+++ src/routes/resources.ts
@@ -3,7 +3,7 @@ import * as controller from '../controllers/resources';
 import {checkJwt} from "../middlewares/checkJWT";
 const router = express.Router();
 
-router.get('/resources/:id([0-9]+)', [checkJwt],controller.getResource);
+router.get('/resources/:id([0-9]+)',controller.getResource);
 router.get('/resources/', controller.getResources);
 router.put('/resources/:id([0-9]+)/versioning/commit', [checkJwt], controller.putResourceVersion)
 
